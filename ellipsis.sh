2f7284f45c3e78fa823b83eb5766334f885ba161
Nadeem Bitar <nadeem@gmail.com>
2018-05-26 22:52:15 -0700

    :package: Add opam packages

--- ellipsis.sh
+++ ellipsis.sh
@@ -3,9 +3,16 @@
 # shinzui/ocaml ellipsis package
 
 # The following hooks can be defined to customize behavior of your package:
-# pkg.install() {
-#     fs.link_files $PKG_PATH
-# }
+pkg.install() {
+  brew install ocaml opam
+
+  opam install fpath yaml core utop yojson core_extended core_bench \
+    cohttp
+
+  opam install merlin ocp-indent ocp-broswer
+
+  
+}
 
 # pkg.push() {
 #     git.push
